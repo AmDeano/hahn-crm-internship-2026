8a9b7eef8875cc57ec4150238b759c8b13d7388c
Abdelilah Melhaoui <153937839+AmDeano@users.noreply.github.com>
2025-12-14 23:35:55 +0100

    Add Application Configuration

--- backend/src/main/java/com/internship/taskmanager/security/JwtUtil.java
+++ backend/src/main/java/com/internship/taskmanager/security/JwtUtil.java
@@ -1,5 +1,9 @@
 package com.internship.taskmanager.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
